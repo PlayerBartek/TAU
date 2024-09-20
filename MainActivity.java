67dd723a6d6c5fc3fbf197449372283a9b6c89fb
PlayerBartek <161807072+PlayerBartek@users.noreply.github.com>
2024-09-20 13:54:39 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -12,19 +12,21 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity{
 
     TextView txtCount;
-
+    //zmienna countera
     int licz = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //wczytywanie zmiennej po wznowieniu
         if (savedInstanceState != null) {
             licz = savedInstanceState.getInt("licz");
         }
-
+        //counter
         txtCount = findViewById(R.id.textView);
         txtCount.setText("Kliknięto "+String.valueOf(licz)+" razy");
+        //przycisk do wyłączania przycisku i zwiększania countera o 1
         Button buttonDisable = findViewById(R.id.buttonDisable);
         buttonDisable.setOnClickListener(v -> {
             buttonDisable.setEnabled(false);
@@ -32,9 +34,13 @@ public class MainActivity extends AppCompatActivity{
             txtCount.setText("Kliknięto "+String.valueOf(licz)+" razy");
             Toast.makeText(MainActivity.this, "Kliknięto przycisk",Toast.LENGTH_SHORT).show();
         });
+
+        //przycisk do włączania przycisku wyżej
         Button buttonEnable = findViewById(R.id.buttonEnable);
         buttonEnable.setOnClickListener(v -> buttonDisable.setEnabled(true));
 
+
+        //przycisk do resetowania licznika
         Button buttonReset = findViewById(R.id.buttonReset);
         buttonReset.setOnClickListener(v -> {
             licz = 0;
@@ -43,7 +49,7 @@ public class MainActivity extends AppCompatActivity{
 
 
     }
-
+    //zapisywanie kliknieć
     @Override
     protected void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
